d0320af12c9e619ee01bf1a7d998e05f845cf69e
username <email@example.com>
2021-06-07 21:38:21 +0200

    Bekend

---
+++ src/main/java/rva/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package rva.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rva.jpa.Grupa;
+import rva.jpa.Student;
+
+public interface StudentRepository extends JpaRepository <Student, Integer>{
+		Collection<Student> findByGrupa(Grupa g);
+//		Collection<Student> findByBrojIndeksaContainingIgnoreCase(String brojIndeksa);
+		
+		Collection<Student> findByIdLessThanOrderById(Integer id);
+}
