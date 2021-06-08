661d58f130cbd3d83812a76add3de6dc44b42511
username <email@example.com>
2021-06-08 22:36:41 +0200

    Endpoint

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
-export const PROJEKAT_URL= 'http://localhost:8083/projekat';
+/*export const PROJEKAT_URL= 'http://localhost:8083/projekat';
 
 export const SMER_URL= 'http://localhost:8083/smer';
 
@@ -7,3 +7,10 @@ export const GRUPA_URL= 'http://localhost:8083/grupa';
 export const STUDENT_URL= 'http://localhost:8083/student';
 
 export const G_STUDENT_URL= 'http://localhost:8083/studentGrupaID';
+
+*/
+export const PROJEKAT_URL= 'https://rva-backend-gr2.herokuapp.com/projekat';
+export const SMER_URL= 'https://rva-backend-gr2.herokuapp.com/smer';
+export const GRUPA_URL= 'https://rva-backend-gr2.herokuapp.com/grupa';
+export const G_STUDENT_URL= 'https://rva-backend-gr2.herokuapp.com/studentGrupaID';
+export const STUDENT_URL= 'https://rva-backend-gr2.herokuapp.com/student'; 
