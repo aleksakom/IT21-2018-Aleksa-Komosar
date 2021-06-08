d11a9a84847b8bf070ced557d2c68a2d5d4a4a82
username <email@example.com>
2021-06-08 22:51:51 +0200

    Izmene linka

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,8 +9,8 @@ export const STUDENT_URL= 'http://localhost:8083/student';
 export const G_STUDENT_URL= 'http://localhost:8083/studentGrupaID';
 
 */
-export const PROJEKAT_URL= 'https://rva-backend-gr2.herokuapp.com/projekat';
-export const SMER_URL= 'https://rva-backend-gr2.herokuapp.com/smer';
-export const GRUPA_URL= 'https://rva-backend-gr2.herokuapp.com/grupa';
-export const G_STUDENT_URL= 'https://rva-backend-gr2.herokuapp.com/studentGrupaID';
-export const STUDENT_URL= 'https://rva-backend-gr2.herokuapp.com/student'; 
+export const PROJEKAT_URL= 'https://backend-it21-2018.herokuapp.com//projekat';
+export const SMER_URL= 'https://backend-it21-2018.herokuapp.com//smer';
+export const GRUPA_URL= 'https://backend-it21-2018.herokuapp.com//grupa';
+export const G_STUDENT_URL= 'https://backend-it21-2018.herokuapp.com//studentGrupaID';
+export const STUDENT_URL= 'https://backend-it21-2018.herokuapp.com//student'; 
