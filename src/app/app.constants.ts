af8aeca26890aa1fc98a4cd514553c26344591c3
username <email@example.com>
2021-06-09 10:43:04 +0200

    Test2

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,6 +9,8 @@ export const STUDENT_URL= 'http://localhost:8083/student';
 export const G_STUDENT_URL= 'http://localhost:8083/studentGrupaID';
 
 */
+/*komentar
+*/
 export const PROJEKAT_URL= 'https://backend-it21-2018.herokuapp.com//projekat';
 export const SMER_URL= 'https://backend-it21-2018.herokuapp.com//smer';
 export const GRUPA_URL= 'https://backend-it21-2018.herokuapp.com//grupa';
